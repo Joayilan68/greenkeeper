8bb8c86b5de08e0199ae3ca034d7e9a27cac487e
Joayilan68 <jordankrebs1@gmail.com>
2026-06-07 09:55:27 +0200

    Fix MyLawn

--- src/pages/MyLawn.jsx
+++ src/pages/MyLawn.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useUser } from "@clerk/clerk-react";
 import { useProfile } from "../lib/useProfile";
